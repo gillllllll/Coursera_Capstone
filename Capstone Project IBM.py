9e5d1f2bd62fdef160451b5cc22957f628dab3da
gillllllll <47506959+gillllllll@users.noreply.github.com>
2020-12-14 13:52:55 -0500

    Add files via upload

---
+++ Capstone Project IBM.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h2> Capstone Project Workbook</h2>
+# <h4> This notebook will be used for Coursera's Applied Data Science Capstone course. </h4>
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
